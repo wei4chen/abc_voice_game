8ed691a3b71555723c7da638134afba49fc0867e
wei4chen <166278246+wei4chen@users.noreply.github.com>
2026-05-25 09:57:58 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/abc_voice_game/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
